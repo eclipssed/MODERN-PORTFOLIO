078b2a2ee232a0467812779320a4c3c9362fb1c4
eclipssed <your_email@example.com>
2024-06-06 14:41:34 +0500

    fixed issue in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex mt-16 md:flex-row flex-col gap-20 items-center">
         <p className="md:text-base text-sm md:font-normal font-light text-blue-100">
           Copyright © 2024 Furqan
         </p>
